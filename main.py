9b090102ea8229793aa3377b5bf81c481f7f80e4
Xavier Boudreau <xavi.boudreau@gmail.com>
2020-04-10 11:45:03 -0400

    Add last exercise

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 import sched
 import time
 
-workout2 = ['planks', 'push ups', 'sit ups']
-
 workout = ['Leg raise clap', 'Reverse Crunch', \
             'Spider man plank', 'Cross body mountain climbers', \
             'Russian Twist', 'In and Out', \
@@ -40,36 +38,13 @@ def announce_exercise(currTime, exercise_name, warning_length = 15):
 
 
 if __name__ == '__main__':
-    '''
-    workout starts in 10 seconds, starting with planks
-    GO!
-    15 seconds left, then pushups
-
-    epoch = time.time() + LOADTIME
-    currTime = epoch
-    s.absenter(currTime, DEFAULT_PRIORITY, say, ('workout starts in {} seconds'.format(WAITIME)))
-    currTime += WAITIME
-
-    for i in range(len(workout)-3, 2):
-        s.absenter(currTime, DEFAULT_PRIORITY, say, (workout[i]))
-        announce_exercise(currTime)
-        currTime += EXERCISE_LENGTH
-        s.absenter(currTime, DEFAULT_PRIORITY, say, (workout[i+1]))
-        announce_break(currTime)
-        currTime += EXERCISE_LENGTH
-        s.absenter(currTime, DEFAULT_PRIORITY, say, ('break, get ready for {}'.format(workout[i+2])))
-        currTime += BREAK_LENGTH
-
-
-    '''
-
     s = sched.scheduler(time.time, time.sleep)
     epoch = time.time() + LOADTIME
     currTime = epoch
     s.enterabs(currTime, DEFAULT_PRIORITY, say, ('workout starts in {} seconds'.format(WAITTIME)))
     currTime += WAITTIME
 
-    for i in range(0, len(workout)-3, 2):
+    for i in range(0, len(workout)-1, 2):
         s.enterabs(currTime, DEFAULT_PRIORITY, say, (workout[i]))
         announce_exercise(currTime, workout[i+1])
         currTime += EXERCISE_LENGTH
@@ -79,5 +54,10 @@ if __name__ == '__main__':
         s.enterabs(currTime, DEFAULT_PRIORITY, say, ('10 second break, get ready for {}'.format(workout[i+2])))
         currTime += BREAK_LENGTH
 
-    print(len(s.queue))
+    # Last exercise is standalone
+    s.enterabs(currTime, DEFAULT_PRIORITY, say, ('{} this is the last workout'.format(workout[-1])))
+    announce_exercise(currTime, 'we are done')
+    currTime += EXERCISE_LENGTH
+    s.enterabs(currTime, DEFAULT_PRIORITY, say, ('We are done. Great workout'))
+
     s.run()
